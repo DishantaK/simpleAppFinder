b0de6e1f80544621e7bceb270aee0ca735a24d4f
shagnar <nykiakpatrick@gmail.com>
2019-04-23 18:48:51 -0400

    initial add

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('main').innerHTML = `
+
+<h6>You are using :  ${window.navigator.appCodeName}</h6>
+<h6>Your Language is : ${window.navigator.language}</h6>
+<h6> Your OS is : ${window.navigator.platform}</h6>
+
+
+
+`;
